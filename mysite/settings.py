f86c5a56cab248b3c3a8a90e7af5af6aef32346a
Jared Dantis <jareddantis@gmail.com>
2022-07-07 15:30:30 +0800

    Move template dir to root

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'unchained/templates',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
